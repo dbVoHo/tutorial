49b8739f0b07a0d7bf9381d87e8cff129bbf8716
Volker Hoff <volker.hoff-extern@deutschebahn.com>
2022-03-30 15:35:03 +0200

    world5

--- src/assets/favicon-32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="favicon-32x32" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../../itrain-frontend/src/assets/favicon/favicon-32x32.png" width="32" height="32"/>
-</tileset>
